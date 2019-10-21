4843cd3de3b707d0e48abdcb2c3435ecafc7c103
FrogTravel <e.levchenko@innopolis.ru>
2019-10-21 16:54:43 +0300

    Detecting of blue color

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ import cv2
 
 down_sample_factor = 10
 
+
+def detect_blue_color(img):
+    img_copy = img.copy()
+    img_red, img_green, img_blue = cv2.split(img_copy)
+
+    for i in range(0, img_copy.shape[0]):
+        for j in range(0, img_copy.shape[1]):
+            if 20 < img_red[i, j] < 50 and \
+                    40 < img_green[i, j] < 60 and \
+                    60 < img_blue[i, j] < 255:
+                img_copy[i, j] = 255
+            else:
+                img_copy[i, j] = 0
+
+    return img_copy
+
+
 if __name__ == '__main__':
     cap = cv2.VideoCapture('cup.ogv')
 
@@ -14,20 +31,12 @@ if __name__ == '__main__':
         frame_copy = frame.copy()
         frame_copy = cv2.cvtColor(frame_copy, cv2.COLOR_BGR2RGB)
         frame_copy = cv2.resize(frame_copy, (int(frame_copy.shape[1] / down_sample_factor),
-                                int(frame_copy.shape[0] / down_sample_factor)))
-        img_red, img_green, img_blue = cv2.split(frame_copy)
+                                             int(frame_copy.shape[0] / down_sample_factor)))
 
-        for i in range(0, frame_copy.shape[0]):
-            for j in range(0, frame_copy.shape[1]):
-                if 20 < img_red[i, j] < 50 and \
-                        40 < img_green[i, j] < 60 and \
-                        60 < img_blue[i, j] < 255:
-                    frame_copy[i, j] = 255
-                else:
-                    frame_copy[i, j] = 0
+        img_blue_detected = detect_blue_color(frame_copy)
 
-        cv2.imshow('Video', frame_copy)
-        cv2.imshow('Blue', img_blue)
+        cv2.imshow('Video', frame)
+        cv2.imshow('Blue', img_blue_detected)
         ch = 0xFF & cv2.waitKey(1)
         if ch == 27:
             break
